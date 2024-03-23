50f660474d5a8341faff4cc11d3c6b0096a6a58d
Karner David <viEclipsekd@gmail.com>
2024-03-23 17:23:46 -0500

    Initial Commit

---
+++ SaveBetter/SaveBetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveBetterApp.swift
+//  SaveBetter
+//
+//  Created by Karner David on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SaveBetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
